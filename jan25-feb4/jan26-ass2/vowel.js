a7175631eefdab89a6ed0b398ab70b212b5a8b58
Bhalke@priya <97160531+BhalkePriya11@users.noreply.github.com>
2022-03-05 06:26:49 +0530

    Add files via upload

---
+++ jan25-feb4/jan26-ass2/vowel.js
@@ -0,0 +1,19 @@
+var character='E';
+if(character=='A'){
+    console.log("vowel");
+}
+else if(character=='E'){
+    console.log("vowel");
+}
+else if(character=='I'){
+    console.log("vowel");
+}
+else if(character=='O'){
+    console.log("vowel");
+}
+else if(character=='U'){
+    console.log("vowel");
+}
+else{
+ console.log("it is consonant");
+}
