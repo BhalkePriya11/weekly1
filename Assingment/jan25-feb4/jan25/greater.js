86a02a9a3246b93bc515e847d53e2a58075c494e
Bhalke@priya <97160531+BhalkePriya11@users.noreply.github.com>
2022-03-05 06:11:21 +0530

    Add files via upload

---
+++ Assingment/jan25-feb4/jan25/greater.js
@@ -0,0 +1,10 @@
+var a=10;
+ if(a>10){
+    console.log("number is greater than 10");
+}
+ else(a<10){
+    console.log("number is less than 10");
+}
+ else{
+    console.log("number is equall to 10");
+}
